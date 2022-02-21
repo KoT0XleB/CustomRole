c00eb56676c89144e56d10f0a6717e455ecc403e
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2022-02-21 23:32:16 +0500

    Add files via upload

---
+++ CustomRole/Config.cs
@@ -0,0 +1,49 @@
+﻿using Qurre.API.Addons;
+using Qurre.API.Objects;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CustomRole
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "CustomRole";
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+        [Description("List for creating a custom roles (u can make 10+ roles). For example, janitor:")]
+        public List<MakeRole> MakingRoles { get; set; } = new List<MakeRole>()
+        {
+            new MakeRole()
+            {
+                name_role = "Janitor",
+                info_console = "<color=yellow>You're a janitor. You clean up here. You have more HP and a janitor card.</color>",
+                broadcast_role = "<color=yellow>You're a janitor. You clean up here.\n You have more HP and a janitor card.</color>",
+                items_role = 
+                {
+                    ItemType.KeycardJanitor,
+                    ItemType.Flashlight
+                },
+                class_role = RoleType.ClassD,
+                hp_role = 120,
+                who_canbe = RoleType.ClassD,
+                spawn_chance_role = 100,
+                spawn_room = RoomType.LczToilets,
+                howmuchpeople_canbe = 1
+            }
+        };
+        public class MakeRole
+        {
+            public string name_role { get; set; }
+            public string info_console { get; set; }
+            public string broadcast_role { get; set; }
+            public List<ItemType> items_role { get; set; } = new List<ItemType>() { };
+            public RoleType class_role { get; set; }
+            public int hp_role { get; set; }
+            public RoleType who_canbe { get; set; }
+            public int spawn_chance_role { get; set; }
+            public RoomType spawn_room { get; set; }
+            public int howmuchpeople_canbe { get; set; }
+        }
+    }
+}
---
+++ CustomRole/CustomRole.cs
@@ -0,0 +1,107 @@
+﻿using MEC;
+using Qurre;
+using Qurre.API.Events;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Player = Qurre.API.Player;
+using Random = UnityEngine.Random;
+
+namespace CustomRole
+{
+    public class CustomRole : Plugin
+    {
+        public override string Developer => "KoT0XleB#4663";
+        public override string Name => "CustomRole";
+        public override Version Version => new Version(1, 0, 0);
+        public override int Priority => 20;
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public Config CustomConfig { get; private set; }
+        public void RegisterEvents() 
+        {
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Round.Start += OnRoundStarted;
+            Qurre.Events.Player.Dead += OnDead;
+            Qurre.Events.Player.RoleChange += OnRoleChanging;
+        }
+        public void UnregisterEvents()
+        {
+            CustomConfigs.Remove(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Round.Start -= OnRoundStarted;
+            Qurre.Events.Player.Dead -= OnDead;
+            Qurre.Events.Player.RoleChange -= OnRoleChanging;
+        }
+        public void OnRoundStarted()
+        {
+            foreach (var list in CustomConfig.MakingRoles)
+            {
+                if (list.spawn_chance_role >= Random.Range(0, 100))
+                {
+                    var people_count = list.howmuchpeople_canbe;
+                    while (people_count > 0)
+                    {
+                        Player player = GetRandomPlayer();
+                        if (player.Team != Team.SCP && player.Role == list.who_canbe)
+                        {
+                            PlayerList.Contains(player);
+
+                            player.ClearBroadcasts();
+                            player.Broadcast(list.broadcast_role, 10);
+                            player.SendConsoleMessage(list.info_console, "green");
+
+                            player.Role = list.class_role;
+                            player.RoleName = list.name_role;
+                            player.MaxHp = list.hp_role;
+                            player.Hp = player.MaxHp;
+
+                            foreach (var item in list.items_role)
+                            {
+                                player.AddItem(item);
+                            }
+
+                            Timing.CallDelayed(1f, () =>
+                            {
+                                player.TeleportToRoom(list.spawn_room);
+                            });
+                        }
+                        PlayerList.Remove(player);
+                        people_count--;
+                    }
+                }
+            }
+        }
+        public void OnDead(DeadEvent ev)
+        {
+            if (PlayerList.Contains(ev.Target))
+            {
+                PlayerList.Remove(ev.Target);
+                ev.Target.RoleName = string.Empty;
+            }
+        }
+        public void OnRoleChanging(RoleChangeEvent ev)
+        {
+            if (PlayerList.Contains(ev.Player))
+            {
+                PlayerList.Remove(ev.Player);
+                ev.Player.RoleName = string.Empty;
+            }
+        }
+        public Player GetRandomPlayer()
+        {
+            Player player = Player.Get(Random.Range(2, Player.List.Count()));
+            if (!PlayerList.Contains(player))
+            {
+                return player;
+            }
+            else return GetRandomPlayer();
+        }
+        public List<Player> PlayerList { get; set; } = new List<Player>();
+    }
+}
